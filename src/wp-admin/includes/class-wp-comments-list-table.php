bismaps/wp-primaconsulting

commit 609277a39b63b7ec8eb6b4356c1b8828e6751499

List Table: Restore the List Table items property to allow Bulk Actions to be properly determined.
This fixes the display of Bulk Actions where there exists only a single page of results.

Props shaneeckert, jobthomas, dd32.
Fixes #45028.


git-svn-id: https://develop.svn.wordpress.org/trunk@43671 602fd350-edb4-49c9-b593-d223f7449a82

src/wp-admin/includes/class-wp-comments-list-table.php
@@ -508,8 +508,14 @@ class WP_Comments_List_Table extends WP_List_Table {
 
 	<tbody id="the-extra-comment-list" data-wp-lists="list:comment" style="display: none;">
 		<?php
+			/*
+			 * Back up the items to restore after printing the extra items markup. 
+			 * The extra items may be empty, which will prevent the table nav from displaying later.
+			 */
+		 	$items = $this->items;
 			$this->items = $this->extra_items;
 			$this->display_rows_or_placeholder();
+			$this->items = $items;
 		?>
 	</tbody>
 

commit 178a874d9431acc3ad36c13160c52b7e64798a0a

List Table: Fix formatting issue introduced in r43671.
Props dd32.
See #45028.


git-svn-id: https://develop.svn.wordpress.org/trunk@43672 602fd350-edb4-49c9-b593-d223f7449a82

src/wp-admin/includes/class-wp-comments-list-table.php
@@ -509,10 +509,10 @@ class WP_Comments_List_Table extends WP_List_Table {
 	<tbody id="the-extra-comment-list" data-wp-lists="list:comment" style="display: none;">
 		<?php
 			/*
-			 * Back up the items to restore after printing the extra items markup. 
+			 * Back up the items to restore after printing the extra items markup.
 			 * The extra items may be empty, which will prevent the table nav from displaying later.
 			 */
-		 	$items = $this->items;
+			$items       = $this->items;
 			$this->items = $this->extra_items;
 			$this->display_rows_or_placeholder();
 			$this->items = $items;