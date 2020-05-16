bismaps/wp-primaconsulting

commit 271d63619c46f7d861e77d433d0ac9d05d305419

REST API: Add @since entries for rest_validate_value_from_schema().
See #49572, #48818, #44949, #50053, #48820, #49720, #42961, #44975, #43392, #38583.


git-svn-id: https://develop.svn.wordpress.org/trunk@47811 602fd350-edb4-49c9-b593-d223f7449a82

src/wp-includes/rest-api.php
@@ -1227,6 +1227,13 @@ function rest_get_avatar_sizes() {
  * Validate a value based on a schema.
  *
  * @since 4.7.0
+ * @since 4.9.0 Support the "object" type.
+ * @since 5.2.0 Support validating "additionalProperties" against a schema.
+ * @since 5.3.0 Support multiple types.
+ * @since 5.4.0 Convert an empty string to an empty object.
+ * @since 5.5.0 Add the "uuid" and "hex-color" formats.
+ *              Support the "minLength", "maxLength" and "pattern" keywords for strings.
+ * 				Validate required properties.
  *
  * @param mixed  $value The value to validate.
  * @param array  $args  Schema array to use for validation.

commit 15f20f72827ebfc3178deee9c8c3d2c5c92c16da

Coding Standards: Correct docblock indentation.
Fixes PHPCS failure introduced in r47811.


git-svn-id: https://develop.svn.wordpress.org/trunk@47812 602fd350-edb4-49c9-b593-d223f7449a82

src/wp-includes/rest-api.php
@@ -1233,7 +1233,7 @@ function rest_get_avatar_sizes() {
  * @since 5.4.0 Convert an empty string to an empty object.
  * @since 5.5.0 Add the "uuid" and "hex-color" formats.
  *              Support the "minLength", "maxLength" and "pattern" keywords for strings.
- * 				Validate required properties.
+ *              Validate required properties.
  *
  * @param mixed  $value The value to validate.
  * @param array  $args  Schema array to use for validation.